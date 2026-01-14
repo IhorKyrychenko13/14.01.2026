e75d986a349bb18b8e787244785a0f2b8dd36bee
Ihor Kyrychenko <legouflandia@gmail.com>
2026-01-14 16:37:58 +0100

    .

--- 14.01.2026.cpp
+++ 14.01.2026.cpp
@@ -1,5 +1,11 @@
 #include <iostream>
+#include "Catalogue.h"
 int main()
 {
+	Catalogue catalog;
 
+	catalog.addEddition(new Book("C++", "StrausTroop", true, 2000, "Ranok"));
+	catalog.addEddition(new Magazine("Nature", "World", true, 2026, 1));
+	catalog.addEddition(new TextBook("Math", "Noname", true, 2026, "Ranok", 11));
+	catalog.showCatalogue();
 }
